7cae145a1d68deab35984c281003240ddc1b3633
Magne Syljuåsen <66528035+magnesyljuasen@users.noreply.github.com>
2022-05-15 12:58:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-mkdir -p ~/.streamlit/
+#mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"email@domain\"\n\
-" > ~/.streamlit/credentials.toml
+#echo "\
+#[general]\n\
+#email = \"email@domain\"\n\
+#" > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=true\n\
-port = 8501\n\
-" >> ~/.streamlit/config.toml
+#echo "\
+#[server]\n\
+#headless = true\n\
+#enableCORS=true\n\
+#port = 8501\n\
+#" >> ~/.streamlit/config.toml
